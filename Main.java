a4ba64bd3d2244b81ceaab9c04aa41b2b50eb147
rdNoir <84554345+rdNoir@users.noreply.github.com>
2021-06-01 13:07:00 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,29 +1,44 @@
-
 package dsassignment;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
-public class DSAssignment <T> {
+/*
+Update Notes:
+1/6  - Changed type parameter from String to Integer.
+*/
 
-
+public class Main <T> {
+    static int N, C;
+    static Graph<Integer> graph;
+    
     public static void main(String[] args) {
         
-        int N, C = 0;
-        Graph<String, Integer> graph = new Graph<>();
-        String[] vertices = null;
+        buildGraph(); //accept graph and store as a graph
+        
+        System.out.println("Greedy Simulation");
+        GreedySearch<Integer> test = new GreedySearch(graph, C);
+        System.out.println(graph.calculateTour(test.pathList));
+        
+        System.out.println("\nMCTS Simulation");
+        MCTSSearch<Integer> test2 = new MCTSSearch(graph, C);
+        System.out.println(graph.calculateTour(test2.pathList));
+        
+    }
+    
+    public static void buildGraph() {
+        graph = new Graph<>();
+        int[] vertices = null;
         
         try{
             try (Scanner scan = new Scanner(new FileInputStream("input.txt"))) {
                 N = scan.nextInt();
                 C = scan.nextInt();
-                vehicle.setCapacity(C);
-                int i =0;
-                vertices = new String[N];
+                int i = 0;
+                vertices = new int[N];
                 while(scan.hasNext()) {
-                    vertices[i] = String.valueOf(i);
+                    vertices[i] = i;
                     //Adds a vertex that has (ID, xCoordinate, yCoordinate, demandSize)
                     graph.addVertex(vertices[i], scan.nextInt(), scan.nextInt(), scan.nextInt());
                     i++;
@@ -33,13 +48,6 @@ public class DSAssignment <T> {
             System.out.println("File Not Found");
         }
         
-        //Testing 
-        System.out.println(graph.getAllVertexObjects());
-        for (String v: vertices) {
-            System.out.println("Coordinates of " + v + " :" + graph.getXCor(v) + " " + graph.getYCor(v) + " Demand Size: " + graph.getDemandSize(v));
-        }
-        
-        
         //To create connected edges
         for (int i=0; i<graph.getSize(); i++) {
             for (int j=0; j<graph.getSize(); j++) {
@@ -47,11 +55,5 @@ public class DSAssignment <T> {
                     graph.addEdge(vertices[i], vertices[j]);
             }
         }
-        
-
-        
     }
-    
-    
-    
 }
