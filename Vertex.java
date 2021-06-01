5de984cfeb1f6c9835694ff816bfdd411fb41f15
rdNoir <84554345+rdNoir@users.noreply.github.com>
2021-06-01 13:06:00 +0800

    Removed N type parameter.

--- Vertex.java
+++ Vertex.java
@@ -1,29 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dsassignment;
 
-public  class Vertex<T extends Comparable<T>, N extends Comparable <N>> {
-        T vertexInfo;
-        int xCor;
-        int yCor;
-        int demand;
-        Vertex<T,N> nextVertex;
-        Edge<T,N> firstEdge;
-        
-        public Vertex() {
-            vertexInfo = null;
-            nextVertex = null;
-            firstEdge = null;
-        }
-        public Vertex(T vInfo, Vertex<T,N> next, int x, int y, int d) {
-            vertexInfo = vInfo;
-            nextVertex = next;
-            firstEdge = null;
-            xCor = x;
-            yCor = y;
-            demand = d;
-        }    
+public  class Vertex<T extends Comparable<T>> {
+    T vertexInfo;
+    int xCor;
+    int yCor;
+    int demand;
+    Vertex<T> nextVertex;
+    Edge<T> firstEdge;
+
+    public Vertex() {
+        vertexInfo = null;
+        nextVertex = null;
+        firstEdge = null;
     }
+    
+    public Vertex(T vInfo, Vertex<T> next, int x, int y, int d) {
+        vertexInfo = vInfo;
+        nextVertex = next;
+        firstEdge = null;
+        xCor = x;
+        yCor = y;
+        demand = d;
+    }    
+}
