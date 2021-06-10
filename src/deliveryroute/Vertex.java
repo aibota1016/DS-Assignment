deea10618bf5242bd191cea2057ffe30980729b9
Aibota Sanatbyek <59278608+aibota1016@users.noreply.github.com>
2021-06-10 11:10:49 +0800

    Update Vertex.java

--- src/deliveryroute/Vertex.java
+++ src/deliveryroute/Vertex.java
@@ -7,19 +7,27 @@ public  class Vertex<T extends Comparable<T>> {
     int demand;
     Vertex<T> nextVertex;
     Edge<T> firstEdge;
+    
+    T parent;
+    double f = 0;
+    double g = 0;
+    double h = 0;
 
     public Vertex() {
         vertexInfo = null;
         nextVertex = null;
         firstEdge = null;
+        parent = null;
     }
     
     public Vertex(T vInfo, Vertex<T> next, int x, int y, int d) {
         vertexInfo = vInfo;
         nextVertex = next;
         firstEdge = null;
+        parent = null;
         xCor = x;
         yCor = y;
         demand = d;
-    }    
+    }
+
 }
