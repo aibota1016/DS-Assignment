e6b34569e7c8abe82b4f4fa3741981210992ad3b
Aibota Sanatbyek <59278608+aibota1016@users.noreply.github.com>
2021-06-10 11:09:18 +0800

    Update Graph.java

--- src/deliveryroute/Graph.java
+++ src/deliveryroute/Graph.java
@@ -9,6 +9,7 @@ import javafx.animation.TranslateTransition;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
@@ -20,7 +21,7 @@ public class Graph <T extends Comparable<T>> {
     Vertex <T> head;
     int size;
     static TextArea textArea = new TextArea();
-    
+    static TextField text = new TextField(); //for displaying the cycle
     
     public Graph() {
         head = null;
@@ -77,6 +78,106 @@ public class Graph <T extends Comparable<T>> {
             }
          }
         return -1;
+    }
+    
+       public double getH(T v) {
+        if (hasVertex(v) == true) {
+            Vertex<T> temp = head;
+            while(temp!= null) {
+                if (temp.vertexInfo.compareTo(v) == 0)
+                    return temp.h;
+                temp =temp.nextVertex;
+            }
+         }
+        return -1;
+    }
+    
+    public double getF(T v) {
+        if (hasVertex(v) == true) {
+            Vertex<T> temp = head;
+            while(temp!= null) {
+                if (temp.vertexInfo.compareTo(v) == 0)
+                    return temp.f;
+                temp =temp.nextVertex;
+            }
+         }
+        return -1;
+    }
+    
+    public double getG(T v) {
+        if (hasVertex(v) == true) {
+            Vertex<T> temp = head;
+            while(temp!= null) {
+                if (temp.vertexInfo.compareTo(v) == 0)
+                    return temp.g;
+                temp =temp.nextVertex;
+            }
+        }
+        return -1;
+    }
+    
+    public T getParent(T v) {
+        if (hasVertex(v) == true) {
+            Vertex<T> temp = head;
+            while(temp!= null) {
+                if (temp.vertexInfo.compareTo(v) == 0)
+                    return temp.parent;
+                temp =temp.nextVertex;
+            }
+        }
+        return null;
+    }
+    
+    public void setH(T v, double h) {
+        if (hasVertex(v) == true) {
+            Vertex<T> temp = head;
+            while(temp!= null) {
+                if (temp.vertexInfo.compareTo(v) == 0) {
+                    temp.h = h;
+                    return;
+                }
+                temp = temp.nextVertex;
+            }
+         }
+    }
+    
+    public void setF(T v, double f) {
+        if (hasVertex(v) == true) {
+            Vertex<T> temp = head;
+            while(temp!= null) {
+                if (temp.vertexInfo.compareTo(v) == 0) {
+                    temp.f = f;
+                    return;
+                }
+                temp = temp.nextVertex;
+            }
+         }
+    }
+    
+    public void setG(T v, double g) {
+        if (hasVertex(v) == true) {
+            Vertex<T> temp = head;
+            while(temp!= null) {
+                if (temp.vertexInfo.compareTo(v) == 0) {
+                    temp.g = g;
+                    return;
+                }
+                temp = temp.nextVertex;
+            }
+         }
+    }
+    
+    public void setParent(T v, T parent) {
+        if (hasVertex(v) == true) {
+            Vertex<T> temp = head;
+            while(temp!= null) {
+                if (temp.vertexInfo.compareTo(v) == 0) {
+                    temp.parent = parent;
+                    return;
+                }
+                temp = temp.nextVertex;
+            }
+        }
     }
     
     public boolean addVertex(T v, int xCor, int yCor, int d) {
@@ -216,16 +317,20 @@ public class Graph <T extends Comparable<T>> {
         return vertex;
     }
     
-    public void drawVisitedEdges(ArrayList<ArrayList<T>> vertices) { //changes the color of visited edges and animates
+    public int get2dListSize(ArrayList<ArrayList<T>> list) {
         int n = 0;
-        for (int i=0; i<vertices.size(); i++) {
-            n += vertices.get(i).size();
+        for (int i=0; i<list.size(); i++) {
+            n += list.get(i).size();
         }
+        return n;
+    }
+    
+    public void drawVisitedEdges(ArrayList<ArrayList<T>> vertices) { //changes the color of visited edges and animates
+        int n = get2dListSize(vertices);
         final IntegerProperty i = new SimpleIntegerProperty(0);
         final IntegerProperty j = new SimpleIntegerProperty(0);
-        
-        Timeline timeline = new Timeline();
-        timeline.getKeyFrames().add(
+        Timeline timelineEdges = new Timeline();
+        timelineEdges.getKeyFrames().add(
             new KeyFrame(
                 Duration.seconds(1),
                 event -> {
@@ -234,29 +339,54 @@ public class Graph <T extends Comparable<T>> {
                             Line edge  = drawEdge(vertices.get(i.get()).get(j.get()), vertices.get(i.get()).get(j.get()+1));
                             edge.setStroke(Color.rgb(182, 162, 132));
                             j.set(j.get() + 1);   
-                            
                         } else {
-                            timeline.playFromStart();
+                            timelineEdges.playFromStart();
                             j.set(0);
                             i.set(i.get() + 1);   
                         }
-                    } 
+                    }
+                    if (Main.reset) {
+                        timelineEdges.stop(); //stop the simulation if reset button is clicked
+                    }
                 } 
             )
         );
-        timeline.setDelay(Duration.seconds(1));
-        timeline.setCycleCount(n);
-        timeline.play(); 
+        timelineEdges.setDelay(Duration.seconds(1));
+        timelineEdges.setCycleCount(n);
+        timelineEdges.play(); 
+    }
+    
+    //displays cycle count on the screen
+    public void setCycleText(ArrayList<ArrayList<T>> vertices) {
+        int n = get2dListSize(vertices);
+        final IntegerProperty count = new SimpleIntegerProperty(1);
+        final int length = n; 
+        Timeline timelineText = new Timeline();
+        timelineText.getKeyFrames().add(
+            new KeyFrame(
+                Duration.seconds(1.25),
+                event -> {
+                    text.setText("Cycle: " +count.getValue().toString());
+                    if (count.get() < length) {
+                        count.set(count.get() + 1);
+                    }
+                    if (Main.reset) {
+                        timelineText.stop(); 
+                    }
+                } 
+            )
+        );
+        timelineText.setDelay(Duration.seconds(1));
+        timelineText.setCycleCount(n);
+        timelineText.play(); 
     }
     
     public void drawVisitedVertices(ArrayList<ArrayList<T>> vertices) { //changes the color of visited vertices and animates
-        int n = 0;
-        for (int i=0; i<vertices.size(); i++) {
-            n += vertices.get(i).size();
-        }
+        int n = get2dListSize(vertices);
         final IntegerProperty i = new SimpleIntegerProperty(0);
         final IntegerProperty j = new SimpleIntegerProperty(0);
-        Timeline timeline = new Timeline(
+        Timeline timelineVertices = new Timeline();
+        timelineVertices.getKeyFrames().add(
             new KeyFrame(
                 Duration.seconds(1),
                 event -> {
@@ -270,34 +400,34 @@ public class Graph <T extends Comparable<T>> {
                             j.set(0);
                             i.set(i.get() + 1);   
                         }
-                    } 
+                    }
+                    if (Main.reset) {
+                        timelineVertices.stop(); //stop the simulation if reset button is clicked
+                    }
                 } 
             )
         );
-        timeline.setCycleCount(n+10);
-        timeline.play();    
+        timelineVertices.setCycleCount(n+10);
+        timelineVertices.play();    
     }
     
     public ImageView moveVehicle(ImageView im, T src, T dest) { //animates and moves the vehicle from one vertex to another
-        TranslateTransition translate = new TranslateTransition(Duration.millis(1000), im);
-        translate.setFromX(getXCor(src)*2 + 190);
-        translate.setFromY(getYCor(src)*2 - 20);
-        translate.setToX(getXCor(dest)*2 + 190);
-        translate.setToY(getYCor(dest)*2 - 20);
-        translate.playFromStart();
-        translate.play();
+        TranslateTransition transition = new TranslateTransition(Duration.millis(1000), im);
+        transition.setFromX(getXCor(src)*2 + 190);
+        transition.setFromY(getYCor(src)*2 - 20);
+        transition.setToX(getXCor(dest)*2 + 190);
+        transition.setToY(getYCor(dest)*2 - 20);
+        transition.playFromStart();
+        transition.play();
         return im;
     }
     
     public void vehicleTravel(ArrayList<ArrayList<T>> vertices) { //animates and moves the vehicle through visited vertices/path
-        int n = 0;
-        for (int i=0; i<vertices.size(); i++) {
-            n += vertices.get(i).size();
-        }
+        int n = get2dListSize(vertices);
         FileInputStream input = null;
         try {
             input = new FileInputStream(
-                    "C:\\Users\\hp\\OneDrive\\Documents\\NetBeansProjects\\DeliveryRoute\\src\\deliveryroute\\vehicle2.png");
+                    "C:\\Users\\hp\\OneDrive\\Documents\\NetBeansProjects\\DeliveryRoute\\src\\deliveryroute\\vehicle.png");
         } catch (FileNotFoundException ex) {
             System.out.println("File Not Found");
         }
@@ -306,8 +436,8 @@ public class Graph <T extends Comparable<T>> {
 
         final IntegerProperty i = new SimpleIntegerProperty(0);
         final IntegerProperty j = new SimpleIntegerProperty(0);
-        Timeline timeline = new Timeline();
-        timeline.getKeyFrames().add(
+        Timeline timelineVehicle = new Timeline();
+        timelineVehicle.getKeyFrames().add(
             new KeyFrame(
                 Duration.seconds(1),
                 event -> {
@@ -316,17 +446,20 @@ public class Graph <T extends Comparable<T>> {
                             moveVehicle(vehicle, vertices.get(i.get()).get(j.get()), vertices.get(i.get()).get(j.get()+1));
                             j.set(j.get() + 1);   
                         } else {
-                            timeline.playFromStart();
+                            timelineVehicle.playFromStart();
                             j.set(0);
                             i.set(i.get() + 1);   
                         }
                     } 
+                    if (Main.reset) {
+                        timelineVehicle.stop(); //stop the simulation if reset button is clicked
+                    }
                 } 
             )
         );
-        timeline.setDelay(Duration.seconds(1));
-        timeline.setCycleCount(n);
-        timeline.play(); 
+        timelineVehicle.setDelay(Duration.seconds(1));
+        timelineVehicle.setCycleCount(n);
+        timelineVehicle.play(); 
     }
     
     
@@ -342,7 +475,7 @@ public class Graph <T extends Comparable<T>> {
     public double calculateCost(ArrayList<T> vertices) { //path cost and demand of one vehicle
         textArea.appendText("\n");
         int n = vertices.size();
-
+        
         for (int a=0; a<n; a++) { //prints out the path
             if (a!= (n-1)) {
                 System.out.print(vertices.get(a) + " -> ");
@@ -372,35 +505,37 @@ public class Graph <T extends Comparable<T>> {
         System.out.println("Capacity: " + vehicleLoad);
         textArea.appendText("\nCapacity: " + vehicleLoad);
         System.out.println("Cost: " + pathCost);
-        textArea.appendText("\nCost: " + pathCost);
+        textArea.appendText("\nCost: " + pathCost + "\n");
         
         return pathCost;
     }
     
-    public double calculateTour(ArrayList<ArrayList<T>> arr) { //prints out results of a search
-        Main.root.getChildren().add(textArea);
+    /**
+     * @param arr
+     * @param vehicleID - note: set this parameter to 0 if it's not parallel simulation
+     * @return  the tourCost and prints out results of a search
+     */
+    public double calculateTour(ArrayList<ArrayList<T>> arr, int vehicleID) {
         double tourCost = 0;
-        //defining textarea
-        textArea.setPrefHeight(420);
-        textArea.setPrefWidth(200);
-        textArea.setLayoutX(620);
-        textArea.setVisible(false);
-        int n = arr.size(); //equals to the number of vehicles needed
+        int n = arr.size(); 
         
         drawVisitedVertices(arr);
         drawVisitedEdges(arr);
         vehicleTravel(arr);
-        
+
         for (int j=0; j<n; j++) {
-            System.out.println("Vehicle " + (j+1));
-            tourCost+= calculateCost(arr.get(j));
+            tourCost+= calculateCost(arr.get(j)); //calculating tour cost
         }
-        textArea.clear();
+        //printing empty lines to make the console ouput area look cleaner
+        for(int i = 0; i < 1000; i++) {
+            System.out.println();
+        }
+        textArea.clear(); //clearing the text area
         
-        System.out.println("Tour");
+        System.out.print("Tour");
         textArea.setText("Tour");
         textArea.setVisible(true);
-        
+
         //adding timeline to show output one by one on the screen
         final IntegerProperty j = new SimpleIntegerProperty(0);
         Timeline timeline = new Timeline();
@@ -408,7 +543,13 @@ public class Graph <T extends Comparable<T>> {
                 Duration.seconds(arr.get(j.get()).size()-0.5),
                 event -> {
                     if (j.getValue() < arr.size()) {
-                        textArea.appendText("\nVehicle " + (j.getValue()+1)); 
+                        if (vehicleID == 0) {  //if 0 means, it's not parallel simulation, so increase the vehicle number
+                            System.out.println("\nVehicle " + (j.getValue()+1));
+                            textArea.appendText("\nVehicle " + (j.getValue()+1)); 
+                        } else {  //if parallel, just print the ID of the vehicle
+                            System.out.println("\nVehicle " + vehicleID);
+                            textArea.appendText("\nVehicle " + vehicleID); 
+                        }
                         calculateCost(arr.get(j.get()));
                     }
                     j.set(j.get() + 1);  
@@ -418,9 +559,85 @@ public class Graph <T extends Comparable<T>> {
         timeline.setCycleCount(arr.size());
         timeline.play(); 
         
-        System.out.print("Tour Cost: ");
-        textArea.appendText("\nTour Cost: " + tourCost);
+        if (vehicleID == 0) {
+            System.out.println("\nTour Cost: " + tourCost);
+        }
         return tourCost;
     }
+    
+    
+    //Methods below are used for parallel simulation, 
+    //Assuming that we have 3 vehicles, it will be simulated parallelly
+    public void tourParallel(ArrayList<ArrayList<T>> arr) { 
+        //intializing paths of 3 vehicles
+        ArrayList<ArrayList<T>> vehicle1path = new ArrayList<>();
+        ArrayList<ArrayList<T>> vehicle2path = new ArrayList<>();
+        ArrayList<ArrayList<T>> vehicle3path = new ArrayList<>();
+
+        switch (arr.size()) {
+            case 1 -> { //in case of only one route
+                vehicle1path.add(arr.get(0));
+                calculateTour(vehicle1path, 1);
+            }
+            case 2 -> { //in case of two routes
+                vehicle1path.add(arr.get(0));
+                vehicle1path.add(arr.get(1));
+                calculateTour(vehicle1path, 1);
+                calculateTour(vehicle2path, 2);
+            }
+            case 3 -> { //in case of three routes
+                vehicle1path.add(arr.get(0));
+                vehicle1path.add(arr.get(1));
+                vehicle3path.add(arr.get(2));
+                calculateTour(vehicle1path, 1); 
+                calculateTour(vehicle2path, 2);
+                calculateTour(vehicle3path, 3);
+            }
+            default -> { //if more than three routes, the first vehicle that comes to the depot will take the next order
+                for (int i=0; i<arr.size(); i++) {
+                    min(vehicle1path, vehicle2path, vehicle3path).add(arr.get(i)); 
+                }
+            }
+        }
+        
+        double tourCost = calculateTour(vehicle1path, 1) + calculateTour(vehicle2path, 2) + calculateTour(vehicle3path, 3);
+        textArea.appendText("\nTour Cost: " + tourCost);
+        System.out.println("\nTour Cost: " + tourCost);
+        //setting the cycle count limit to the vehicle path with largest number of elememts
+        setCycleText(max(vehicle1path, vehicle2path, vehicle3path)); 
+        
+    }
+    
+    //returns a 2D array with the smallest number of elements
+    public ArrayList<ArrayList<T>> min(ArrayList<ArrayList<T>> a, ArrayList<ArrayList<T>> b, ArrayList<ArrayList<T>> c) {
+        int size1 = get2dListSize(a), size2 = get2dListSize(b), size3 = get2dListSize(c);
+        
+        if (Math.min(Math.min(size1, size2), size3) == size1) 
+            return a;
+        else if (Math.min(Math.min(size1, size2), size3) == size2) 
+            return b;
+        else if (Math.min(Math.min(size1, size2), size3) == size3) 
+            return c;
+        else {
+            return a;
+        }
+    }
+    
+    //returns a 2D array with the largest number of elements
+    public ArrayList<ArrayList<T>> max(ArrayList<ArrayList<T>> a, ArrayList<ArrayList<T>> b, ArrayList<ArrayList<T>> c) {
+        int size1 = get2dListSize(a), size2 = get2dListSize(b), size3 = get2dListSize(c);
+        
+        if (Math.max(Math.max(size1, size2), size3) == size1) 
+            return a;
+        else if (Math.max(Math.max(size1, size2), size3) == size2) 
+            return b;
+        else if (Math.max(Math.max(size1, size2), size3) == size3) 
+            return c;
+        else {
+            return a;
+        }
+    }
+    
+    
     
 }
